e39f1192ee297f5cb44308b8c3e742e5423100ad
CocaColux <marcoserhal@icloud.com>
2022-05-23 15:07:12 +0200

    test

---
+++ js/bender.js
@@ -0,0 +1,33 @@
+   
+function create_robot(){
+    let bender = document.createElement('img'); 
+    bender.src = "../img/Bender.png"; 
+    bender.style.height = "100px"; 
+    bender.style.position = "absolute";
+    bender.style.top = "0px"; 
+    bender.style.left = "0px"; 
+    bender.style.transition = "all 2s"; 
+    bender.id = "bender"; 
+    document.body.append(bender);
+    }
+
+function move_bot_right(){
+    let bender = document.createElement('img'); 
+    bender.style.left = "100px";
+}    
+
+function move_bot_left(){
+    let bender = document.createElement('img'); 
+    bender.style.right = "100px";
+}
+
+function move_bot_down(){
+    let bender = document.createElement('img'); 
+    bender.style.up = "100px";
+}
+
+function move_bot_up(){
+    let bender = document.createElement('img'); 
+    bender.style.down = "100px";
+}
+
