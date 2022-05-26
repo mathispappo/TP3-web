334b382a31aee2f62db002bb5997e2c09e096114
Mathis Pappo <43952106+mathispappo@users.noreply.github.com>
2022-05-26 20:40:16 +0200

    Co-authored-by: Pierre-Marie  <Unaxe@users.noreply.github.com>

--- Partie_3/js/script.js
+++ Partie_3/js/script.js
@@ -6,7 +6,7 @@ async function ajouter(){
   const tache = tacheSaisie()
   pushTache(tache)
 
-  const table = document.querySelector('.datatable')
+  const table = document.querySelector('.datatable tbody')
   console.log(table)
   mesTaches.forEach((tache) => {
     var bol = true
@@ -17,42 +17,45 @@ async function ajouter(){
         }
     }
 
+    if(bol){
 
-    //Vérifier que la récupération se fait bien !)
-    console.log(document.forms.newTaskF.tache.value);
-    console.log(document.forms.newTaskF.date.value);
+      //Vérifier que la récupération se fait bien !)
+      console.log(document.forms.newTaskF.tache.value);
+      console.log(document.forms.newTaskF.date.value);
 
-    //création des variable pour la création d'une nouvelle ligne dans le tableau
-    const newItem = document.createElement('tr')
-    const taskTd = document.createElement('td')
-    const dateTd = document.createElement('td')
-    const categorieTd = document.createElement('td')
-    taskTd.textContent = document.newTaskF.tache.value
-    dateTd.textContent = document.newTaskF.date.value
-    categorieTd.textContent = document.newTaskF.categorie.value
-    
-    //const selectEntree = document.getElementById("entreeId");
-    //const valeurselectionnee = selectEntree.options[selectEntree.selectedIndex].value;
-    //const textselectionne = selectEntree.options[selectEntree.selectedIndex].text;
-   //Vérification de la récupération
-    console.log(taskTd.textContent)
-    console.log(dateTd.textContent)
-    console.log(categorieTd.textContent)
+      //création des variable pour la création d'une nouvelle ligne dans le tableau
+      const newItem = document.createElement('tr')
+      const taskTd = document.createElement('td')
+      const dateTd = document.createElement('td')
+      const categorieTd = document.createElement('td')
+      taskTd.textContent = document.newTaskF.tache.value
+      dateTd.textContent = document.newTaskF.date.value
+      categorieTd.textContent = document.newTaskF.categorie.value
+      
+      //const selectEntree = document.getElementById("entreeId");
+      //const valeurselectionnee = selectEntree.options[selectEntree.selectedIndex].value;
+      //const textselectionne = selectEntree.options[selectEntree.selectedIndex].text;
+    //Vérification de la récupération
+      console.log(taskTd.textContent)
+      console.log(dateTd.textContent)
+      console.log(categorieTd.textContent)
 
-    if (!document.newTaskF.tache.checkValidity() ||
-          !document.newTaskF.date.checkValidity() ||
-          !document.newTaskF.categorie.checkValidity()
-    ) {
-      return
+      if (!document.newTaskF.tache.checkValidity() ||
+            !document.newTaskF.date.checkValidity() ||
+            !document.newTaskF.categorie.checkValidity()
+      ) {
+        return
+      }
+      
+      //const table = document.querySelector('table')
+      newItem.append(taskTd, dateTd, categorieTd)
+
+      /* le premier élément dans le document qui contient la classe "datatable" est retourné*/
+      const table = document.querySelector('.datatable tbody')
+      /*  Ex2)3)vi) */
+      table.appendChild(newItem)
+
     }
-    
-    //const table = document.querySelector('table')
-    newItem.append(taskTd, dateTd, categorieTd)
-
-     /* le premier élément dans le document qui contient la classe "datatable" est retourné*/
-    const table = document.querySelector('.datatable tbody')
-    /*  Ex2)3)vi) */
-    table.appendChild(newItem)
   })
 }
 
@@ -81,12 +84,11 @@ function tacheSaisie() {
 
 document.addEventListener("DOMContentLoaded", () => {
   var maNouvelleTache = {
-    taskTd: document.newTaskF.tache.value,
+    taskTd: document.newTaskF.tache.textContent,
     dateTd: document.newTaskF.date.value,
     categorieTd: document.newTaskF.categorie.value,
-  
+    
     [Symbol.iterator](){
-      console.log(this.taskTd)
       let tableau = Object.values(this);
       let prop = 0;
       
@@ -126,5 +128,7 @@ class Tache {
 let mesTaches = []
 
 function pushTache(tache) {
+  mesTaches.push(tache)
   console.log(mesTaches)
 }
+
